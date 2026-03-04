a1f3694698ff6af380e5115eed92def224045079
artur <artur.sahakyan.2023@bk.ru>
2026-03-04 11:26:14 +0400

    add+hero+card

--- src/app/features/home/why-us/why-us.ts
+++ src/app/features/home/why-us/why-us.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: "app-why-us",
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: "./why-us.html",
   styleUrl: "./why-us.scss",
 })
